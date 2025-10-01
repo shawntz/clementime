285033ca077baf0f557aa68ca0b9abf0e23a0a63
Shawn Schwartz <shawn@shawnschwartz.com>
2025-10-01 13:18:16 -0700

    Create authenticable.rb

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,35 @@
+module Authenticable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_request
+  end
+
+  private
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id]) if @decoded
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: 'Unauthorized' }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: 'Unauthorized' }, status: :unauthorized
+    end
+
+    render json: { errors: 'Unauthorized' }, status: :unauthorized unless @current_user
+  end
+
+  def current_user
+    @current_user
+  end
+
+  def authorize_admin!
+    unless current_user&.admin?
+      render json: { errors: 'Forbidden - Admin access required' }, status: :forbidden
+    end
+  end
+end
